d173fa6f8faeab4c71941dfb32963741dd3a07c7
khyrulAlam <khyrulalam69@gmail.com>
2019-04-03 00:34:48 +0600

    drag and drop list

--- dist/draglists/app.js
+++ dist/draglists/app.js
@@ -1,53 +1,28 @@
 __dragMe = document.querySelectorAll(".dragMe");
 __dropMeHere = document.querySelector("#dropMeHere");
+__dropMeThere = document.querySelector("#dropMeThere");
 
-__dragMe.forEach(element => {
-  element.onmousedown = event => {
-    let highestTop =
-      __dropMeHere.getBoundingClientRect().height -
-      element.getBoundingClientRect().height;
-    let highestLeft =
-      __dropMeHere.getBoundingClientRect().width -
-      element.getBoundingClientRect().width;
-
-    // (2) prepare to moving: make absolute and top by z-index
-    element.style.position = "absolute";
-    element.style.zIndex = 1000;
-
-    let moveAt = (pageX, pageY) => {
-      element.style.left = pageX - element.offsetWidth / 2 + "px";
-      element.style.top = pageY - element.offsetHeight / 2 + "px";
-    };
-
-    let onMouseMove = event => {
-      moveAt(event.pageX, event.pageY);
-    };
-    moveAt(event.pageX, event.pageY);
-
-    // (3) move the dragMe on mousemove
-    document.addEventListener("mousemove", onMouseMove);
-
-    // (4) drop the dragMe, remove unneeded handlers
-    element.onmouseup = function() {
-      document.removeEventListener("mousemove", onMouseMove);
-      element.onmouseup = null;
-      let elementTop =
-        element.getBoundingClientRect().top -
-        __dropMeHere.getBoundingClientRect().top;
-      let elementLeft =
-        element.getBoundingClientRect().left -
-        __dropMeHere.getBoundingClientRect().left;
-      if (
-        elementLeft < 0 ||
-        elementTop < 0 ||
-        highestLeft < elementLeft ||
-        highestTop < elementTop
-      ) {
-        console.log("-- out of box --");
-      }
-    };
-    element.ondragstart = function() {
-      return false;
-    };
+__dragMe.forEach((element, index) => {
+  element.setAttribute("dd", "foo-" + index);
+  element.ondragstart = function(event) {
+    let todoIndex = element.getAttribute("dd");
+    event.dataTransfer.setData("Text", todoIndex);
   };
 });
+
+__dropMeHere.ondrop = function(event) {
+  let todoIndex = event.dataTransfer.getData("text");
+  let domNote = document.querySelector(`[dd=${todoIndex}]`);
+  __dropMeHere.appendChild(domNote);
+};
+__dropMeHere.ondragover = function(event) {
+  return false;
+};
+__dropMeThere.ondrop = function(event) {
+  let todoIndex = event.dataTransfer.getData("text");
+  let domNote = document.querySelector(`[dd=${todoIndex}]`);
+  __dropMeThere.appendChild(domNote);
+};
+__dropMeThere.ondragover = function(event) {
+  return false;
+};
